abeb3d118386c0c0c2f1d18ed4b95c8fcb340afc
5huv0 <md88748874@gmail.com>
2024-12-12 02:00:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import PassChanger from './components/PassChanger'
+
+function App() {
+
+
+  return (
+    <>
+      <PassChanger />
+    </>
+  )
+}
+
+export default App
